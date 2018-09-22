2b8c3cd1a49ca566f7d9f09206782d9f61147ce6
patiboh <patiboh.io@gmail.com>
2018-09-22 11:43:03 +0200

    feat(fix flex styles):

--- js/main.js
+++ js/main.js
@@ -168,22 +168,31 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.alt;
   li.append(image);
 
+  const intro = document.createElement('div');
+  intro.className = 'restaurant-intro';
+
+  const text = document.createElement('div');
+  text.className = 'restaurant-text';
+
   const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  text.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  text.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  text.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  intro.append(text);
+  intro.append(more);
+
+  li.append(intro)
 
   return li
 }
